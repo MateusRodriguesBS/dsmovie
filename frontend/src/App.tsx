4ef3ee193b6b549cf35b377d04057a0e8d192093
Mateus Rodrigues <Mateus_zatch123@yahoo.com.br>
2022-03-08 07:48:21 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import {ReactComponent as GithubIcon} from 'assests/img/github.svg'
-import Navbar from './components/Navbar';
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
