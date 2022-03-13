dda2d6bad5f5a005e50f4cd41651adfefa63a8a9
Mateus Rodrigues <Mateus_zatch123@yahoo.com.br>
2022-03-13 08:55:11 -0300

    Correção

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -33,9 +33,9 @@ function getFills(score: number) {
   }
 
   function Star({fill}: StarProps){
-      if (fill == 0){
+      if (fill === 0){
           return <Starempty/>;
-      }else if(fill == 1){
+      }else if(fill === 1){
         return <Starfull/>
       }else{
         return <Starhalf/>
